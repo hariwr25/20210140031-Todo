ad0ad62ca6aa3343d915f72534c645a53f23e3ae
hariwr <hariwr25@gmail.com>
2025-05-04 22:27:21 +0700

    mengubah routes

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,9 @@
 <?php
 
-use Illuminate\Support\Facades\Routes;
-use App\Http\Controllers\TodoController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\UserController;
-// use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TodoController;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
@@ -14,13 +13,21 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
+Route::middleware(['auth'])->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::resource('todo', TodoController::class)->except(['show']);
+    Route::delete('/todo/completed', [TodoController::class, 'destroyCompleted'])->name('todo.deleteallcompleted');
+    Route::patch('/todo/{todo}/complete', [TodoController::class, 'complete'])->name('todo.complete');
+    Route::patch('/todo/{todo}/uncomplete', [TodoController::class, 'uncomplete'])->name('todo.uncomplete');
 });
 
-Route::get('/todo', [TodoController::class,'view'])->name('todo.view');
+Route::middleware(['auth', 'admin'])->group(function () {
+    Route::resource('user', UserController::class)->except(['show']);
+    Route::patch('/user/{user}/makeadmin', [UserController::class, 'makeadmin'])->name('user.makeadmin');
+    Route::patch('/user/{user}/removeadmin', [UserController::class, 'removeadmin'])->name('user.removeadmin');
+});
 
-Route::get('/user', [UserController::class,'index'])->name('user.index');
 require __DIR__.'/auth.php';
