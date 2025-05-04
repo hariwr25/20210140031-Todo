3d857c728a5b3b1e35d8215e41e067cff13406f5
hariwr <hariwr25@gmail.com>
2025-05-04 22:20:16 +0700

    Menambahkan kode pada Middleware\Admin.php

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -1,17 +1,27 @@
 <?php
- 
- namespace App\Http\Controllers;
- 
- use Illuminate\Http\Request;
- 
- class TodoController extends Controller
- {
-     public function view(){
-         return view("todo.index");
-     }
- 
-     // public function create(){
-     
-     //     return create("todo.index");
-     // }
- }
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Todo;
+use Illuminate\Support\Facades\Auth;
+
+class TodoController extends Controller
+{
+    public function index()
+    {
+        // $todos = Todo::all();
+        $todos = Todo::where('user_id', Auth::id())->get();
+        dd($todos);
+        return view('todo.index');
+    }
+
+    public function view()
+    {
+        return view("todo.index");
+    }
+
+    // public function create(){
+    //     return create("todo.index");
+    // }
+}
